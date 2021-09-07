3c49f9537f0ac0827025e4be3f484557214a1d95
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2021-09-07 22:28:01 +0100

    Added init framework for react-three-fiber implementation

--- showcase/src/App.js
+++ showcase/src/App.js
@@ -1,12 +1,18 @@
 import HeaderCard from './components/HedderCard';
 import DividerRedWaves from './components/Divider-RedWaves';
+import CanvasEnv from './components/three-js-components/canvas';
+
 
 function App() {
   return(
-  <div>
+    <div>
       <HeaderCard />
       <DividerRedWaves />
-   </div> 
+    <p> This next section is a react-three-fibersection</p>
+    
+    <CanvasEnv />
+    
+    </div> 
   );
 }
 
