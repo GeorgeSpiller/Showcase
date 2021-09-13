c6bd9bb127e537632a4db92b1cbf27dc2acffd93
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2021-09-13 17:29:08 +0100

    Fixed spelling mistakes and grammar

--- showcase/src/components/three-js-components/store/TextAttributes.js
+++ showcase/src/components/three-js-components/store/TextAttributes.js
@@ -3,7 +3,7 @@ const projectOne = {
     p1_title_position: [20, 3, 20],
     p1_title_size: 1, 
     
-    p1_body_message: "This project is the website itself. It is coded  in\nReact and uses react-three-fiber (A react renderer for \nthreejs). The goal was to create a website that could\nshowcase the various different projects that I have\nworked on over the years, big and small. I hope you enjoy!\nGithub: GeorgeSpiller/Showcase\n\n                              Please click to continue....",
+    p1_body_message: "This project is the website itself. It is coded  in\nReact and uses react-three-fiber (a react renderer for \nthreejs). The goal was to create a website that could\nshowcase the various different projects that I have\nworked on over the years, big and small. I hope you enjoy!\nLink to GitHub bellow (Showcase).\n\n                              Please click to continue....",
     p1_body_position: [20, 1, 20],
     p1_body_size: 0.4, 
 }
@@ -13,7 +13,7 @@ const projectTwo = {
     p1_title_position: [-10.5, 0, -14],
     p1_title_size: 1, 
     
-    p1_body_message: "This project was made to dynamically\nfetch Tweets from @Arena_51_Gaming\ninorder to compile them into an\nanimated GIF. The goal was to quickly\nand easily be able to produce high \nquality GIF's of the top 5 most recent\ntweets, inorder to display them on our\nlivestreams\n\nGithub: GeorgeSpiller/A51TweetDisplay",
+    p1_body_message: "This project was made to dynamically\nfetch Tweets from @Arena_51_Gaming\to compile them into an\nanimated GIF. The goal was to quickly\nand easily be able to produce high \nquality GIFs of the top 5 most recent\ntweets, in order to display them on our\nlivestreams\n\nLink to GitHub bellow (A51TweetDisplay).",
     p1_body_position: [5, 4, 0],
     p1_body_size: 0.4, 
 }
@@ -23,7 +23,7 @@ const projectThree = {
     p1_title_position: [-45, 30, -10],
     p1_title_size: 10, 
     
-    p1_body_message:"The goal of this project was to create an interface\nbetween Twitch chat (viewers of a Twitch Liverstream)\nand the game console of Portal 2 (by Valve). It allows\nfor people to type commands in Twitch chat, which would\nthen be picked up by a bot using Twitch's API. This command\nwould then be replaced by its source development console\nequivilent, and executed in the game environment through an\nopen telnet server.\nGithub: GeorgeSpiller/Portal2TwitchBot", 
+    p1_body_message:"The goal of this project was to create an interface\nbetween Twitch chat (viewers of a Twitch Livestream)\nand the game console of Portal 2 (by Valve). It allows\npeople to type commands in Twitch chat, which would\nthen be picked up by a bot using Twitch's API. This command\nwould then be replaced by its source development console\nequivilent, and executed in the game environment through an\nopen telnet server.\nLink to GitHub bellow (Portal2TwitchBot).", 
     p1_body_position: [3, 25, 5],
     p1_body_size: 0.4, 
 }
