ddeae068b1004a2599bbc4b3c49e08de0b31d00d
GeorgeSpiller <57860253+GeorgeSpiller@users.noreply.github.com>
2021-09-19 13:08:38 +0100

    Completed some basic cube animations

--- showcase/src/components/three-js-components/Components/CubdeMultiMaterial.js
+++ showcase/src/components/three-js-components/Components/CubdeMultiMaterial.js
@@ -1,40 +1,73 @@
-
-import React, { useRef } from 'react'
-import { useFrame, useLoader  } from '@react-three/fiber'
-import * as THREE from 'three';
-import raw_sample1 from '../../resources/Images/sample1.jpg'
-import raw_sample2 from '../../resources/Images/sample2.jpg'
-import raw_sample3 from '../../resources/Images/sample3.jpg'
-import raw_sample4 from '../../resources/Images/sample4.jpg'
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { Vector3 } from 'three';
 
 
-export default function CubeMultiMaterial(props) {
+export default function CubeMultiMaterial({materialArray}) {
     const ref = useRef()
+    const MAX_PHOTO_NUMBER = 5;
+    const [clickCycle, setClickCycle] = useState(Math.floor(Math.random() * 6));
+    var goalPos;
 
-    const sample1 = useLoader(THREE.TextureLoader, raw_sample1)
-    const sample2 = useLoader(THREE.TextureLoader, raw_sample2)
-    const sample3 = useLoader(THREE.TextureLoader, raw_sample3)
-    const sample4 = useLoader(THREE.TextureLoader, raw_sample4)
-    
-    useFrame(() => {
-        // animation loop
-        ref.current.rotation.x += 0.01; 
-        ref.current.rotation.y += 0.01;
-        ref.current.rotation.z += 0.01;
+    const distance = 30;
+
+    const pos1 = new Vector3(0, distance, 0);
+    const pos2 = new Vector3(distance, 0, 0);
+    const pos3 = new Vector3(0, 0, -distance);
+    const pos4 = new Vector3(-distance, 0, 0);
+    const pos5 = new Vector3(0, distance, 0);
+    const pos6 = new Vector3(0, distance, distance);
+
+    switch(clickCycle) {
+        case 1:
+            goalPos = pos1;
+            break;
+        case 2:
+            goalPos = pos2;
+            break;
+        case 3:
+            goalPos = pos3;
+            break;
+        case 4:
+            goalPos = pos4;
+            break;
+        case 5:
+            goalPos = pos5;    
+            break;
+        case 6:
+            goalPos = pos6;
+            break;
+        default:
+            goalPos = pos1;
+        }
+    console.log(clickCycle)
+
+    useFrame(({ camera }) => {
+        camera.position.lerp(goalPos, 0.01);
+        camera.lookAt(ref.current.position)
+
+        ref.current.rotation.y += 0.001;
+        ref.current.rotation.z += 0.001;
     })
-    
+
     return (
         <mesh 
-            position={[0, 0, -24]} 
+            position={[0, 0, 0]} 
             ref={ref}
-            scale={20}>
+            scale={20}
+            onClick={() => {
+                setClickCycle(clickCycle + 1)
+                if (clickCycle > MAX_PHOTO_NUMBER) {
+                    setClickCycle(1);
+               }
+            }} >
                 <boxGeometry attach="geometry" args={[1, 1, 1]} />
-                <meshStandardMaterial map={sample1} attachArray="material"  />
-                <meshStandardMaterial map={sample2} attachArray="material"  />
-                <meshStandardMaterial map={sample3} attachArray="material"  />
-                <meshStandardMaterial map={sample4} attachArray="material"  />
-                <meshStandardMaterial map={sample1} attachArray="material"  />
-                <meshStandardMaterial map={sample1} attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[0] } attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[1] } attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[2] } attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[3] } attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[4] } attachArray="material"  />
+                <meshStandardMaterial map={ materialArray[5] } attachArray="material"  />
         </mesh>
     )
   }
--- showcase/src/components/three-js-components/Components/PDevCanvasSection.js
+++ showcase/src/components/three-js-components/Components/PDevCanvasSection.js
@@ -1,14 +1,33 @@
-import React, { Suspense, useState } from 'react';
+import React, { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import CubeMultiMaterial from './CubdeMultiMaterial';
+import * as THREE from 'three';
+import { useLoader } from '@react-three/fiber'
 
-export default function PDEVCanvasSection({title, text}) {
+import raw_sample1 from '../../resources/Images/sample1.jpg'
+import raw_sample2 from '../../resources/Images/sample2.jpg'
+import raw_sample3 from '../../resources/Images/sample3.jpg'
+import raw_sample4 from '../../resources/Images/sample4.jpg'
+import raw_sample5 from '../../resources/Images/sample5.jpg'
+import raw_sample6 from '../../resources/Images/sample6.jpg'
+
+
+export default function PDEVCanvasSection(props) {
 
     const [photoNumber, setPhotoNumber] = useState(1);
-    const NUMBER_OF_PHOTOS = 3;
+    const NUMBER_OF_PHOTOS = 6;
+
+    const matArray = [
+        useLoader(THREE.TextureLoader, raw_sample1),
+        useLoader(THREE.TextureLoader, raw_sample2),
+        useLoader(THREE.TextureLoader, raw_sample3),
+        useLoader(THREE.TextureLoader, raw_sample4),
+        useLoader(THREE.TextureLoader, raw_sample5),
+        useLoader(THREE.TextureLoader, raw_sample6)
+    ]
+
     return(
-        <Suspense fallback={<div className="spinner"><div className="spinner-border"></div></div>}>
-        <div className={"CanvasPDevDiv"} onClick={(event) => {
+        <div className={"CanvasPDevDiv"} onClick={() => {
                 setPhotoNumber(photoNumber + 1)
                 if (photoNumber > NUMBER_OF_PHOTOS) {
                     setPhotoNumber(1);
@@ -16,9 +35,8 @@ export default function PDEVCanvasSection({title, text}) {
             }}>
             <Canvas >
                 <ambientLight intensity={1}/>
-                <CubeMultiMaterial />
+                <CubeMultiMaterial materialArray={matArray}/>
             </Canvas>
         </div>
-    </Suspense>
     );
 }
